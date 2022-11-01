967e6c5be79fc90b15959fb05736beac79d4e720
Tyler Eisen <jteisen90@gmail.com>
2022-10-31 21:51:50 -0400

    complex data added

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 console.log("does this work?");
+
+const user=[
+  { name: 'Crystal Burnett',
+    socials: [
+      {
+      smName: 'Twitter',
+      url: 'https://twitter.com'
+    },
+    {
+      smName: 'LinkedIn',
+      url: 'https://linkedin'
+    },
+  
+    ]
+}
+]
