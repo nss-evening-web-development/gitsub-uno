795a75956415c79059051bd32a9ea11dc58ce13a
Tyler Eisen <jteisen90@gmail.com>
2022-11-01 21:20:36 -0400

    Project cards render and startApp added

--- main.js
+++ main.js
@@ -79,3 +79,33 @@ const renderToDom = (divId, htmlToRender) => {
   selectedDiv.innerHTML = htmlToRender;
 };
 
+// Creates Cards for the Projects page
+const projectsOnDom = (array)=> {
+  let domString="";
+  for (const member of array) {
+    domString+=
+    `<div class="card-header">
+        <h3>
+        ${member.name}  
+        </h3>
+    <div class="card-body">
+         ${member.description} 
+        
+        <p class="card-text"> Last Updated:${member.lastUpdated}</p>
+        </div>
+  
+    </div>
+    `;
+    }
+  
+    renderToDom("#root", domString);
+  }
+  
+
+  const startApp = () => {
+    projectsOnDom(projects);
+    
+  }
+  
+  
+  startApp();
